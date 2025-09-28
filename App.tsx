03d6ad94f1570b89f5d1127d0eb7f0196d71717c
CarlM-69 <carlmathewgabay123@gmail.com>
2025-09-29 00:05:42 +0800

    initial update only

--- App.tsx
+++ App.tsx
@@ -1,45 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useRef } from 'react';
+import { Button, View, StyleSheet, Pressable, Image } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import PagerView from 'react-native-pager-view';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+// Screens
+import MapScreen from './src/map/main';
+import SavedLocationsScreen from './src/savedLocations/main';
+import SettingsScreen from './src/settings/main';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
+export default function App() {
+	const pagerRef = useRef<PagerView>(null);
+  
+	const goToPage = (pageIndex: number) => {
+	  pagerRef.current?.setPage(pageIndex);
+	};
+  
+	return (
+		<SafeAreaProvider style={{ flex: 1 }}>
+			<PagerView style={{ flex: 1 }} initialPage={1} ref={pagerRef}>
+				<View><SavedLocationsScreen /></View>
+				<View><MapScreen /></View>
+				<View><SettingsScreen /></View>
+			</PagerView>
+  
+			<View style={styles.navBar}>
+				<Pressable onPress={() => goToPage(0)}>
+					<Image style={styles.navBarButton} source={require("./assets/navbar/map.png")} />
+				</Pressable>
+				<Pressable onPress={() => goToPage(1)}>
+					<Image style={styles.navBarButton} source={require("./assets/navbar/map.png")} />
+				</Pressable>
+				<Pressable onPress={() => goToPage(2)}>
+					<Image style={styles.navBarButton} source={require("./assets/navbar/settings.png")} />
+				</Pressable>
+			</View>
+	  	</SafeAreaProvider>
+	);
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
+  
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+	navBar: {
+		borderTopStartRadius: 20,
+		borderTopEndRadius: 20,
+		backgroundColor: 'red',
+		width: '100%',
+		height: 100,
+		flexDirection: "row",
+	  	justifyContent: "space-evenly",
+	  	padding: 10,
+		position: "relative",
+		display: "flex",
+		alignItems: "center"
+	},
 
-export default App;
+	navBarButton: {
+		top: -5,
+		aspectRatio: 1,
+		height: 50
+	}
+});
